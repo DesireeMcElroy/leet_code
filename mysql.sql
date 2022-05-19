f9cdcb2ab5e0e97eee7d80a69e47405273e02357
DesireeMcElroy <desireeashley90@gmail.com>
2022-05-19 12:59:35 -0500

    additional exercises

--- mysql.sql
+++ mysql.sql
@@ -7,4 +7,17 @@ FROM salaries
 WHERE salary < (SELECT max(salary) FROM salaries);
 
 
---
+-- Write an SQL query to report the name, population, and area of the big countries.
+select name, population, area
+from World
+where area >= 3000000
+or population >= 25000000;
+
+
+-- Write an SQL query to find the ids of products that are both low fat and recyclable.
+select product_id
+from Products
+where low_fats = 'Y'
+and recyclable = 'Y';
+
+-- 
